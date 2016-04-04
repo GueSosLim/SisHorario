98abb12641191503160fda738ce6fc15b1d79203
bizzdark <sandmancity14@gmail.com>
2016-04-04 22:01:25 +0100

    comentariosSemestre

--- SisHorario.Dominio/Semestre.cs
+++ SisHorario.Dominio/Semestre.cs
@@ -11,36 +11,65 @@ namespace SisHorario.Dominio
     /// </summary>
     public class Semestre
     {
+        /// <summary>
+        /// Código de Semestre asignado
+        /// </summary>
         public int CodigoSemestre { get; private set; }
+        /// <summary>
+        /// Nombre del Semestre
+        /// </summary>
         public string NombreSemestre { get; private set;}
+        /// <summary>
+        /// Año del Semestre
+        /// </summary>
         public string AnioSemestre { get; private set;}
+        /// <summary>
+        /// Estado actual del Semestre
+        /// </summary>
         public string EstadoSemestre { get; private set;}
+        /// <summary>
+        /// Codigo del Plan de Estudio
+        /// </summary>
         public int CodigoPlanEstudio { get; private set; }
+        /// <summary>
+        /// Código del Plan de Estudio
+        /// </summary>
         public virtual PlanEstudio CodPlanEstudio { get; private set; }
+        /// <summary>
+        /// Constructor de la clase Semestre
+        /// </summary>
         private Semestre()
         {
 
         }
-
-        public static Semestre Registrar(int ai_cod_semestre, string as_nom_semestre, string as_anio_semestre, PlanEstudio ao_planestudio)
+        /// <summary>
+        /// Método que crea una nueva instancia de la clase Carga personal
+        /// </summary>
+        /// <param name="ai_cod_semestre">Código de carga personal</param>
+        /// <param name="as_nom_semestre">Sección a la que será asignado el personal</param>
+        /// <param name="as_anio_semestre">Código del Personal</param>
+        /// <param name="ao_planestudio">Código del Semestre</param>
+        /// <param name="as_est_semestre">Estado del Semestre</param>
+        /// <returns></returns>
+        public static Semestre Registrar(int ai_cod_semestre, string as_nom_semestre, string as_anio_semestre, PlanEstudio ao_planestudio, string as_est_semestre)
         {
             return new Semestre()
             {
                 CodigoSemestre = ai_cod_semestre,
                 NombreSemestre = as_nom_semestre,
                 AnioSemestre = as_anio_semestre,
-                EstadoSemestre = "ACTIVO",
+                EstadoSemestre = as_est_semestre,
                 CodPlanEstudio = ao_planestudio,
                 CodigoPlanEstudio = ao_planestudio.CodigoPlanEstudio
             };
         }
-
-        public void Actualizar(int ai_cod_semestre, string as_nom_semestre, string as_anio_semestre, PlanEstudio ao_planestudio)
+         
+        public void Actualizar(int ai_cod_semestre, string as_nom_semestre, string as_anio_semestre, PlanEstudio ao_planestudio, string as_est_semestre)
         {
             CodigoSemestre = ai_cod_semestre;
             NombreSemestre = as_nom_semestre;
             AnioSemestre = as_anio_semestre;
-            EstadoSemestre = "ACTIVO";
+            EstadoSemestre = as_est_semestre;
             CodPlanEstudio = ao_planestudio;
             CodigoPlanEstudio = ao_planestudio.CodigoPlanEstudio;
         }
