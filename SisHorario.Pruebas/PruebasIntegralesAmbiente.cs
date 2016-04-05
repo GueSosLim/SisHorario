23bc0298197e952928e92e23c3b4a03c45f935c8
GueSosLim <guesoslim@gmail.com>
2016-04-04 21:48:59 -0500

    pruebas

---
+++ SisHorario.Pruebas/PruebasIntegralesAmbiente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisHorario.Dominio;
+using SisHorario.Infraestructura;
+
+namespace SisHorario.Pruebas
+{
+    [TestClass]
+    public class PruebasIntegralesAmbiente
+    {
+        [TestMethod]
+        public void RegistrarAmbiente()
+        {
+            var lo_ambiente = Ambiente.Registrar(1, "Laboratorio A", "Laboratorio", 30);
+            using (var lo_contexto = new SisHorarioContexto())
+            {
+                lo_contexto.Ambientes.Add(lo_ambiente);
+                lo_contexto.SaveChanges();
+            }
+            Assert.IsTrue(lo_ambiente.CodigoAmbiente == 1);
+        }
+    }
+}
