73c5cb6634f50c285522f79f345782d9d032cdbe
FernandoJ27 <fjjc27@gmail.com>
2016-04-06 20:53:56 -0500

    Dominio - Repositorios

---
+++ SisHorario.Dominio/IRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisHorario.Dominio
+{
+    interface IRepositorio<T> : IRepositorioLectura<T>
+    {
+        bool Adicionar(T ao_entidad);
+        bool Actualizar(T ao_entidad);
+        bool GuardarCambios();
+    }
+}
