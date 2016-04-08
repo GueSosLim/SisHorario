90b13a150d79b52f3fadff8764c96dbe97a24af0
Lyly Sos <gueiyali@hotmail.com>
2016-04-08 00:30:26 -0500

    pu ciclo

---
+++ SisHorario.Pruebas/PruebaUnitariaCiclo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisHorario.Dominio;
+
+namespace SisHorario.Pruebas
+{
+    [TestClass]
+    public class PruebaUnitariaCiclo
+    {
+        [TestMethod]
+        public void CicloCreadoSatisfactoriamente()
+        {
+            var lo_ciclo = Ciclo.Registrar(1, "I Ciclo");
+            Assert.IsNotNull(lo_ciclo);
+            Assert.IsTrue(lo_ciclo.CodigoCiclo == 1);
+        }
+    }
+}
