e16e45bd42eb8e899f9bf1f071738056b96d80c2
ro6drigo <ro6drigo@hotmail.com>
2016-04-10 22:54:56 -0500

    Mapeo Hora

--- SisHorario.Arquitecturas/Mapeos/HoraMapeo.cs
+++ SisHorario.Arquitecturas/Mapeos/HoraMapeo.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using SisHorario.Dominio;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SisHorario.Arquitecturas.Mapeos
 {
-    class HoraMapeo
+    public class HoraMapeo : EntityTypeConfiguration<Hora>
     {
+        public HoraMapeo()
+        {
+            ToTable("HORA");
+            HasKey(k => k.CodigoHora);
+            Property(p => p.CodigoHora).HasColumnName("COD_HORA").IsRequired();
+            Property(p => p.DescripcionHora).HasColumnName("DES_HORA").IsRequired();
+        }
     }
 }
