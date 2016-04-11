42036ae81f0b872eaf5f32cba3c6470314f74e6d
bizzdark <sandmancity14@gmail.com>
2016-04-11 02:07:23 -0500

    mapeo ambiente

--- SisHorario.Arquitecturas/Mapeos/AmbienteMapeo.cs
+++ SisHorario.Arquitecturas/Mapeos/AmbienteMapeo.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using SisHorario.Dominio;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SisHorario.Arquitecturas.Mapeos
 {
-    class AmbienteMapeo
+    class AmbienteMapeo : EntityTypeConfiguration<Ambiente>
     {
+        public AmbienteMapeo()
+        {
+            ToTable("AMBIENTE");
+            HasKey(k => k.CodigoAmbiente);
+            Property(p => p.CodigoAmbiente).HasColumnName("COD_AMBIENTE").IsRequired();
+            Property(p => p.CapacidadAmbiente).HasColumnName("CAP_AMBIENTE").IsRequired();
+            Property(p => p.NombreAmbiente).HasColumnName("NOM_AMBIENTE").IsRequired();
+            Property(p => p.TipoAmbiente).HasColumnName("TIP_AMBIENTE").IsRequired();
+        }
     }
 }
